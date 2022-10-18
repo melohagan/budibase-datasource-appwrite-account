9106f20f0f7e66f0ae9cefb133ef0417475cc35b
Mel O'Hagan <mel@budibase.com>
2022-10-18 13:47:21 +0100

    Users instead of account

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,96 +1,94 @@
 import { IntegrationBase } from "@budibase/types"
-import { Client, Account } from "appwrite";
+import { Client, Users } from "node-appwrite";
 
 class CustomIntegration implements IntegrationBase {
   private readonly client: Client
-  private readonly account: Account
+  private readonly users: Users
 
-  constructor(config: { endpoint: string; projectId: string; }) {
+  constructor(config: { endpoint: string; projectId: string; apiKey: string; }) {
     this.client = new Client()
-    this.account = new Account(this.client)
-
+    this.users = new Users(this.client)
     this.client
     .setEndpoint(config.endpoint)
     .setProject(config.projectId)
+    .setKey(config.apiKey)
   }
 
-  async create(query: { userId: string, email: string; password: string; name: string; }) {
-    return await this.account.create(query.userId, query.email, query.password, query.name)
+  async create(query: { userId: string; name: string; email: string; phone: string; password: string; passwordVersion: string; extra: { [key: string]: string } }) {
+    if (query.extra.type === "Bcrypt") {
+      return await this.users.createBcryptUser(query.userId, query.email, query.password, query.name)
+    }
+    if (query.extra.type === "MD5") {
+      return await this.users.createMD5User(query.userId, query.email, query.password, query.name)
+    }
+    if (query.extra.type === "Argon2") {
+      return await this.users.createArgon2User(query.userId, query.email, query.password, query.name)
+    }
+    if (query.extra.type === "SHA") {
+      return await this.users.createSHAUser(query.userId, query.email, query.password, query.passwordVersion, query.name)
+    }
+    if (query.extra.type === "PHPass") {
+      return await this.users.createPHPassUser(query.userId, query.email, query.password, query.name)
+    }
+    return await this.users.create(query.userId, query.email, query.phone, query.password, query.name)
   }
 
-  async createEmailSession(query: { email: string; password: string; }) {
-    return await this.account.createEmailSession(query.email, query.password);
-  }
-
-  async createOAuth2Session(query: { provider: string; success: string; failure: string; }) {
-    return this.account.createOAuth2Session(query.provider, query.success, query.failure);
-  }
-
-  async createMagicURLSession(query: { userId: string; email: string; url: string; }) {
-    return await this.account.createMagicURLSession(query.userId, query.email, query.url);
-  }
-
-  async createPhoneSession(query: { userId: string; phone: string; }) {
-    return await this.account.createPhoneSession(query.userId, query.phone);
-  }
-
-  async createAnonymousSession() {
-    return await this.account.createAnonymousSession();
-  }
-
-  async createJWT() {
-    return await this.account.createJWT();
-  }
-
-  async read(query: { extra: { [key: string]: string } }) {
+  async read(query: { userId: string; extra: { [key: string]: string } }) {
+    if (!query.userId) {
+      return await this.users.list()
+    }
     if (query.extra.type === "Preferences") {
-     return await this.account.getPrefs(); 
+      return await this.users.getPrefs(query.userId)
     }
     if (query.extra.type === "Sessions") {
-      return await this.account.listSessions();
+      return await this.users.listSessions(query.userId)
+    }
+    if (query.extra.type === "Memberships") {
+      return await this.users.listMemberships(query.userId)
     }
     if (query.extra.type === "Logs") {
-      return await this.account.listLogs();
+      return await this.users.listLogs(query.userId)
     }
-    return await this.account.get();
+    return await this.users.get(query.userId)
   }
 
-  async readBySessionId(query: { sessionId: string; }) {
-    return await this.account.getSession(query.sessionId)
-  }
-
-  async update(query: { name: string; email: string; phone: string; password: string; }) {
-    let result = {}
+  async update(query: { userId: string; name: string; email: string; number: string; password: string; status: string; emailVerification: string; phoneVerification: string; }) {
+    let response
+    if (query.status) {
+      response = await this.users.updateStatus(query.userId, query.status === "true")
+    }
+    if (query.phoneVerification) {
+      response = await this.users.updatePhoneVerification(query.userId, query.phoneVerification === "true")
+    }
     if (query.name) {
-      result = await this.account.updateName(query.name)
+      response = await this.users.updateName(query.userId, query.name)
+    }
+    if (query.password) {
+      response = await this.users.updatePassword(query.userId, query.password)
     }
     if (query.email) {
-      result = await this.account.updateEmail(query.email, query.password)
+      response = await this.users.updateEmail(query.userId, query.email)
     }
-    if (query.phone) {
-      result = await this.account.updatePhone(query.phone, query.password)
+    if (query.number) {
+      response = await this.users.updatePhone(query.userId, query.number)
     }
-    return result
+    if (query.emailVerification) {
+      response = await this.users.updateEmailVerification(query.userId, query.emailVerification === "true")
+    }
+    if (response) {
+      return response
+    }
+    throw new Error("No properties provided to update!")
   }
 
-  async updatePassword(query: { password: string; oldPassword: string }) {
-    return await this.account.updatePassword(query.password, query.oldPassword)
-  }
-
-  async updatePrefs(query: { json: object }) {
-    return await this.account.updatePrefs(query.json)
-  }
-
-  async updateStatus() {
-    return await this.account.updateStatus()
-  }
-
-  async deleteSession(query: { sessionId: string }) {
-    return await this.account.deleteSession(query.sessionId)
-  }
-
-  async deleteAllSessions() {
-    return await this.account.deleteSessions()
+  async delete(query: { userId: string; sessionId: string; extra: { [key: string]: string } }) {
+    if (query.extra.type === "Sessions") {
+      if (query.sessionId) {
+        return await this.users.deleteSession(query.userId, query.sessionId)
+      }
+      return await this.users.deleteSessions(query.userId)
+    }
+    return await this.users.delete(query.userId)
   }
 }
 
